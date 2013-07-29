05f611358d57aa25ddf1f13536cf4765e088426c
Andrés J. Díaz <ajdiaz@connectical.com>
2013-07-29 18:32:12 +0200

    Document and PEP-008 for switch

--- mico/util/switch.py
+++ mico/util/switch.py
@@ -2,11 +2,14 @@
 # -*- encoding: utf-8 -*-
 # vim:fenc=utf-8:
 
+"""This module provide a switcher, which when is accesses toggle an internal
+status.
+"""
+
 class Switcher(object):
     """Switch a global status using the environment as critical region, and
     setted using class constructor.
     """
-
     _switch = (None, None)
 
     def __init__(self, init_value = None):
@@ -26,10 +29,14 @@ class Switcher(object):
 
     @staticmethod
     def getValue(key):
+        """Class method to get the value of an specified switcher using key.
+        """
         return env.get(key, None)
 
     @classmethod
     def from_key(cls, key, value):
+        """Class method to create a new switcher using key and value.
+        """
         return type.__new__(type,"switch_%s" % key, (Switcher,),{ "_switch": (key, value) })
 
 
